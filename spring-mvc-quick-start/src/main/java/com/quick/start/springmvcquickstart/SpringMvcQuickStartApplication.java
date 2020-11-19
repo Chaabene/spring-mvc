ece38283f4440edd1591f867f8531ab970f2373b
unknown <aymen.chaabene.isi@gmail.com>
2020-11-19 13:52:11 +0100

    initial commit

---
+++ spring-mvc-quick-start/src/main/java/com/quick/start/springmvcquickstart/SpringMvcQuickStartApplication.java
@@ -0,0 +1,13 @@
+package com.quick.start.springmvcquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcQuickStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcQuickStartApplication.class, args);
+	}
+
+}
---
+++ spring-mvc-quick-start/src/test/java/com/quick/start/springmvcquickstart/SpringMvcQuickStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quick.start.springmvcquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcQuickStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
